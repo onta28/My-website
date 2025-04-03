aa507965c046415eed84648cacf866d2b48835f6
onta28 <142404242+onta28@users.noreply.github.com>
2025-04-03 10:06:39 +0700

    Add files via upload

---
+++ json.js
@@ -0,0 +1,23 @@
+// json to javascript object
+let Myjson=`{
+    "name":"onta",
+    "age":22,
+    "phone":null,
+    "status":true
+}`;
+//  json parse -> json string -> js object
+let opj=JSON.parse(Myjson);
+console.log(opj)
+ 
+// javascript object to json
+let ob=
+    {
+        name:"onta",
+        age:22,
+        phone:null,
+        status:true
+    }
+let oj=JSON.stringify(ob.name);
+console.log(oj);
+let text=document.getElementById("json");
+text.innerHTML=ob.name;
